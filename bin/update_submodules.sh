8835c3c243a1dbdc8c004447f18a2d7caf39751a
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-12-14 23:52:57 -0800

    Add Equilibria-cpp as submodule

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -4,12 +4,12 @@
 git submodule update --init --recursive
 
 # update monero-cpp
-cd ./external/monero-cpp
+cd ./external/equilibria-cpp
 git checkout tags/v0.3.3
 git pull --ff-only origin tags/v0.3.3
 
 # update monero-core
-cd ./external/monero-core
-git checkout tags/v0.16.0.1
-git pull --ff-only origin tags/v0.16.0.1
+cd ./external/equilibria
+git checkout v7.0.1
+git pull --ff-only origin v7.0.1
 cd ../../../../
